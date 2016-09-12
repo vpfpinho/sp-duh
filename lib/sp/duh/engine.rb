cfcdc55d05f2c51c7840df4847e5d793deb705b7
Pedro Rodrigues <pedro.rodrigues@cldware.com>
2016-09-12 17:14:17 +0100

    Prepare the gem to support database migrations via `rails g migration`

--- lib/sp/duh/engine.rb
+++ lib/sp/duh/engine.rb
@@ -4,6 +4,12 @@ module SP
   module Duh
     class Engine < ::Rails::Engine
       isolate_namespace SP::Duh
+
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match root.to_s
+          app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+        end
+      end
     end
   end
 end
