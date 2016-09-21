29495549cea58ec5a53b25330664839c390e4e0f
Pedro Rodrigues <pedro.rodrigues@cldware.com>
2016-09-21 12:20:52 +0100

    Fixed the shard_table_data function to ensure only records from the public table are selected, and to add better logging

--- db/functions/sharding/shard_table_data.sql
+++ db/functions/sharding/shard_table_data.sql
@@ -19,19 +19,41 @@ BEGIN
     p_where_clause := 'company_id = %3$L';
   END IF;
 
-  p_insert_queries := p_insert_queries || regexp_replace(format('INSERT INTO %1$I.%2$I (SELECT * FROM public.%2$I WHERE ' || p_where_clause || ')', p_schema_name, p_table, p_company_id), '\s+', ' ', 'gn');
+  RAISE NOTICE '% || %', p_where_clause, regexp_replace(format(p_where_clause, p_schema_name, p_table, p_company_id), '''', '''''', 'gn');
+
+  p_insert_queries := p_insert_queries || regexp_replace(format('
+    SELECT common.execute_and_log_count(
+      ''INSERT INTO %1$I.%2$I (SELECT * FROM ONLY public.%2$I WHERE %4$s)'',
+      ''Inserted %% rows from table public.%2$s into %1$s.%2$s'',
+      ''NOTICE''
+    );',
+    p_schema_name,
+    p_table,
+    p_company_id,
+    regexp_replace(format(p_where_clause, p_schema_name, p_table, p_company_id), '''', '''''', 'gn')
+  ), '\s+', ' ', 'gn');
 
   -- Store the sharded records into a separate table
   IF sharding.table_exists(format('sharded.%1$I', p_table)) THEN
-    query := regexp_replace(format('INSERT INTO sharded.%2$I (SELECT * FROM public.%2$I WHERE ' || p_where_clause || ')', p_schema_name, p_table, p_company_id), '\s+', ' ', 'gn');
+    query := regexp_replace(format('INSERT INTO sharded.%2$I (SELECT * FROM ONLY public.%2$I WHERE ' || p_where_clause || ')', p_schema_name, p_table, p_company_id), '\s+', ' ', 'gn');
   ELSE
-    query := regexp_replace(format('CREATE TABLE sharded.%2$I AS SELECT * FROM public.%2$I WHERE ' || p_where_clause, p_schema_name, p_table, p_company_id), '\s+', ' ', 'gn');
+    query := regexp_replace(format('CREATE TABLE sharded.%2$I AS SELECT * FROM ONLY public.%2$I WHERE ' || p_where_clause, p_schema_name, p_table, p_company_id), '\s+', ' ', 'gn');
   END IF;
 
   p_insert_queries := p_insert_queries || query;
 
   -- And build the delete sharded records from the original table query (only, not from new inherited), to return from the function
-  p_delete_queries := array_prepend(regexp_replace(format('DELETE FROM ONLY public.%2$I WHERE ' || p_where_clause, p_schema_name, p_table, p_company_id), '\s+', ' ', 'gn'),p_delete_queries);
+  p_delete_queries := array_prepend(regexp_replace(format('
+    SELECT common.execute_and_log_count(
+      ''DELETE FROM ONLY public.%2$I WHERE %4$s'',
+      ''Deleted %% rows from table public.%2$s for company %3$s'',
+      ''NOTICE''
+    );',
+    p_schema_name,
+    p_table,
+    p_company_id,
+    regexp_replace(format(p_where_clause, p_schema_name, p_table, p_company_id), '''', '''''', 'gn')
+  ), '\s+', ' ', 'gn'), p_delete_queries);
 
   RETURN;
 -- EXCEPTION
