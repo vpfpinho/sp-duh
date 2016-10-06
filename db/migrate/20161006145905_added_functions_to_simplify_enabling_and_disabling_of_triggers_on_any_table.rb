f37c18d55fd4b7e62513fafd2c4ad442f19820ad
Pedro Rodrigues <pedro.rodrigues@cldware.com>
2016-10-06 16:05:45 +0100

    Added functions to simplify enabling and disabling of triggers on any table

---
+++ db/migrate/20161006145905_added_functions_to_simplify_enabling_and_disabling_of_triggers_on_any_table.rb
@@ -0,0 +1,63 @@
+class AddedFunctionsToSimplifyEnablingAndDisablingOfTriggersOnAnyTable < ActiveRecord::Migration
+  def up
+    execute <<-'SQL'
+      CREATE OR REPLACE FUNCTION common.enable_triggers_on_table(
+        IN        table_name       TEXT,
+        VARIADIC  trigger_names    TEXT[]
+      )
+      RETURNS BOOLEAN AS $BODY$
+      DECLARE
+        table_schema TEXT;
+        trigger_name TEXT;
+      BEGIN
+
+        SELECT "name", "schema"
+        FROM common.get_table_schema_and_name(table_name)
+        INTO table_name, table_schema;
+
+        FOREACH trigger_name IN ARRAY trigger_names LOOP
+          EXECUTE format($$
+            ALTER TABLE %1$I.%2$I
+            ENABLE TRIGGER %3$I;
+          $$, table_schema, table_name, trigger_name);
+        END LOOP;
+
+        RETURN TRUE;
+      END;
+      $BODY$ LANGUAGE plpgsql;
+    SQL
+
+    execute <<-'SQL'
+      CREATE OR REPLACE FUNCTION common.disable_triggers_on_table(
+        IN        table_name       TEXT,
+        VARIADIC  trigger_names    TEXT[]
+      )
+      RETURNS BOOLEAN AS $BODY$
+      DECLARE
+        table_schema TEXT;
+        trigger_name TEXT;
+      BEGIN
+
+        SELECT "name", "schema"
+        FROM common.get_table_schema_and_name(table_name)
+        INTO table_name, table_schema;
+
+        FOREACH trigger_name IN ARRAY trigger_names LOOP
+          EXECUTE format($$
+            ALTER TABLE %1$I.%2$I
+            DISABLE TRIGGER %3$I;
+          $$, table_schema, table_name, trigger_name);
+        END LOOP;
+
+        RETURN TRUE;
+      END;
+      $BODY$ LANGUAGE plpgsql;
+    SQL
+  end
+
+  def down
+    execute %Q[DROP FUNCTION IF EXISTS common.enable_triggers_on_table(TEXT, VARIADIC TEXT[]);]
+    execute %Q[DROP FUNCTION IF EXISTS common.disable_triggers_on_table(TEXT, VARIADIC TEXT[]);]
+
+  end
+end
