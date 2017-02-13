9f3d517517fa13cf64683d8fdd33930cc843b27e
Americo Gomes <ajdgomes.wrk@gmail.com>
2017-02-13 11:25:31 +0000

    Updated TMP currencies i18n sql

--- config/currencies/currencies_i18n.sql
+++ config/currencies/currencies_i18n.sql
@@ -17,7 +17,6 @@ INSERT INTO i18n(key, en) SELECT CONCAT(lower(key),'_major_plural'), name_plural
 INSERT INTO i18n (key, pt, en) VALUES ('currency_value_spellout', 
 	'{3} {0, plural, =1 {{1}} other {{2}}}{4, plural, =0 {} other { e {7} {4, plural, =1 {{5}} other {{6}}}}}',
 	'{3} {0, plural, =1 {{1}} other {{2}}}{4, plural, =0 {} other { and {7} {4, plural, =1 {{5}} other {{6}}}}}'
-
 );
 
 INSERT INTO i18n (key, pt, en) VALUES ('eur_minor_singular', 'cêntimo' , 'cent' );
@@ -29,32 +28,72 @@ INSERT INTO i18n (key, en) VALUES ('usd_minor_plural', 'cents');
 INSERT INTO i18n (key, en) VALUES ('gbp_minor_singular', 'pence' );
 INSERT INTO i18n (key, en) VALUES ('gbp_minor_plural', 'pence');
 
-CREATE OR REPLACE FUNCTION currency_spellout (a_value REAL, a_currency_iso_code TEXT, a_locale TEXT)
-    RETURNS text AS
+CREATE OR REPLACE FUNCTION currency_spellout (a_value NUMERIC, a_currency_iso_code TEXT, a_locale TEXT)
+    RETURNS TEXT AS
 $BODY$
 DECLARE
-    rows text[];
-    keys text[];
-    major real;
-    minor real;
-    cc text;
-    strresult text;
+    rows            TEXT[];
+    keys            TEXT[];
+    major           NUMERIC;
+    minor           NUMERIC;
+    cc              TEXT;
+    tmp_text        TEXT;
+    locale          TEXT;
+    locale_exists   BOOLEAN;
+    strresult       TEXT;
+    nodp            INT;
 BEGIN
 
+    -- COUNTRY CODE ISO CODE --
     cc   := lower(a_currency_iso_code);
-    keys := '{' || cc || '_major_singular, ' || cc || '_major_plural, ' || cc || '_minor_singular, ' || cc || '_minor_plural, currency_value_spellout}';
-    keys := keys::text[];
+    nodp := 2;
 
-    -- TODO pt -> a_locate
-    rows:=array(
-        SELECT COALESCE(pt, en) FROM i18n
-            JOIN unnest(keys) 
-                WITH ORDINALITY t(key, ord) USING (key)
-            ORDER  BY t.ord
-    );
+    -- SEARCH FOR LOCALE --
+    locale := lower(a_locale);
+    EXECUTE 
+        format('SELECT attname FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = ''%1$s'') AND attname = ''%2$s''','i18n', a_locale) 
+    INTO tmp_text;
+    locale_exists := tmp_text IS NOT NULL;
+    IF FALSE = locale_exists
+    THEN
+        -- locale fallback is language code --
+        IF LENGTH(a_locale) = 5
+        THEN
+            locale := SUBSTR(locale, 1, 2);
+            EXECUTE
+                format('SELECT attname FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = ''%1$s'') AND attname = ''%2$s''','i18n', 
+                    locale
+                )
+            INTO tmp_text;
+            locale_exists := tmp_text IS NOT NULL;
+            IF TRUE = locale_exists
+            THEN
+                locale := locale || ',';
+            ELSE
+                locale := '';
+            END IF;
+        ELSE
+            locale := '';
+        END IF;
+    ELSE
+        locale := locale || ',';
+    END IF;
 
-    major := TRUNC(a_value)::real;
-    minor := (ROUND((a_value - major)::numeric, 2) * 100)::real;
+    RAISE NOTICE 'Locale % -> %', a_locale, locale;
+
+    EXECUTE    
+        'SELECT array( SELECT COALESCE(' || locale || 'en)FROM i18n
+                JOIN unnest(
+                    ARRAY[''' || cc || '_major_singular'', ''' || cc || '_major_plural'', ''' || cc || '_minor_singular'', ''' || cc || '_minor_plural'', ''currency_value_spellout'']
+                ) 
+                    WITH ORDINALITY t(key, ord) USING (key)
+        ORDER  BY t.ord);'
+    INTO rows;
+
+    RAISE NOTICE '%', rows;
+
+    major := TRUNC(a_value)::NUMERIC;
+    minor := (ROUND((a_value - major)::numeric, nodp) * POWER(10, nodp))::NUMERIC;
 
     SELECT spellout FROM pg_cpp_utils_currency_spellout(a_locale, 
         major, rows[1], rows[2], 
