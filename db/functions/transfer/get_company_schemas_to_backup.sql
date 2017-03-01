70d7c826a865ded57576caf671ebfbc407945e07
Jorge Morais <jorge.morais@cldware.com>
2017-03-01 12:34:20 +0000

    Ignore user templates from backups except when requested

--- db/functions/transfer/get_company_schemas_to_backup.sql
+++ db/functions/transfer/get_company_schemas_to_backup.sql
@@ -10,21 +10,28 @@ DECLARE
   query         text;
 BEGIN
 
-  query = FORMAT('
-
+  query := FORMAT('
     WITH accounting_schemas AS (
       SELECT ac.schema_name
         FROM accounting.accounting_companies ac
         WHERE ac.company_id = %1$L
-    ),
-    user_schemas AS (
-      SELECT ut.schema_name
-        FROM accounting.user_templates ut
-        WHERE ut.user_id IN (
-          SELECT user_id FROM transfer.get_company_users(%1$L)
-        )
     )
+  ', company_id);
 
+  IF transfer_user_templates THEN
+    query := query || FORMAT('
+      ,
+      user_schemas AS (
+        SELECT ut.schema_name
+          FROM accounting.user_templates ut
+          WHERE ut.user_id IN (
+            SELECT user_id FROM transfer.get_company_users(%1$L)
+          )
+      )
+    ', company_id);
+  END IF;
+
+  query := query || FORMAT('
     SELECT c.schema_name::text, ''main''::text AS schema_type
       FROM companies c
       WHERE c.id = %1$L AND COALESCE(c.schema_name, '''') <> ''''
@@ -33,7 +40,6 @@ BEGIN
       FROM accounting_schemas c
     UNION
     SELECT (SELECT * FROM transfer.get_meta_schema_name(%1$L)) AS schema_name, ''meta''::text AS schema_type
-
   ', company_id);
 
   IF transfer_user_templates THEN
