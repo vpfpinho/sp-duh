bfb9ed761e6a95b7f35ee9992607dfa981c161bd
Jorge Morais <jorge.morais@cldware.com>
2017-03-10 10:55:29 +0000

    Handle errors on backup

--- lib/sp/duh/db/transfer/backup.rb
+++ lib/sp/duh/db/transfer/backup.rb
@@ -9,18 +9,21 @@ module SP
 
         class Backup
 
+          attr_reader :status
+
           def initialize(source_pg_connection)
             @connection = source_pg_connection
           end
 
           def execute(company_id, dump_file = nil)
+            self.status = :ok
             @company_id = company_id
             yield(:before_execute) if block_given?
             before_execute
             yield(:do_execute) if block_given?
-            do_execute(dump_file)
+            do_execute(dump_file) if self.status == :ok
             yield(:after_execute) if block_given?
-            after_execute
+            after_execute if self.status == :ok
           end
 
           protected
@@ -40,12 +43,24 @@ module SP
               dump_file = "#{Time.now.strftime('%Y%m%d%H%M')}_c#{@company_id}.dump"  if dump_file.nil?
               command = "pg_dump -Fc -O --quote-all-identifiers --data-only -n #{@schemas.join(' -n ')} -h #{@connection.host} -p #{@connection.port} -U #{@connection.user} #{@connection.db} > #{dump_file}"
               SP::Duh::Db::Transfer.log_with_time command
-              result = %x[ #{command} ]
+              %x[ #{command} ]
+              if $?.exitstatus != 0
+                File.delete dump_file
+                self.status = :error_dumping_company
+              end
             end
 
             def after_execute
               @ended_at = Time.now
               SP::Duh::Db::Transfer.log_with_time "FINISHED backing up company #{@company_id} in #{(@ended_at - @started_at).round(2)}s"
+            end
+
+            def status=(value)
+              @status = value
+              if value != :ok
+                @ended_at = Time.now
+                SP::Duh::Db::Transfer.log_with_time "CANCELED backing up company #{@company_id} in #{(@ended_at - @started_at).round(2)}s"
+              end
             end
 
         end
