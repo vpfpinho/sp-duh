d95c236000bb5f97b7761cebd2d16d01e93010bc
Joana Alexandra Neves <joana.neves@cldware.com>
2017-11-08 16:41:50 +0000

    Fixing function merge_jsonb_with_arrays_of_keys_and_values to work when first argument IS NULL

--- db/functions/sharding/merge_jsonb_with_arrays_of_keys_and_values.sql
+++ db/functions/sharding/merge_jsonb_with_arrays_of_keys_and_values.sql
@@ -10,13 +10,13 @@ DECLARE
   query TEXT;
   result JSONB;
 BEGIN
-  -- RAISE NOTICE 'sharding.merge_jsonb_with_arrays_of_keys_and_values(%, %, %)', p_jsonb, p_keys, p_values;
+  -- RAISE DEBUG 'sharding.merge_jsonb_with_arrays_of_keys_and_values(%, %, %)', p_jsonb, p_keys, p_values;
 
   query := $$SELECT format('{ %1$s }', array_to_string(part, ', '))
     FROM (
       SELECT array_agg(format('"%1$s": %2$s', "key", "value")) AS part
       FROM (
-        SELECT * FROM jsonb_each_text('$$ || p_jsonb::TEXT || $$')
+        SELECT * FROM jsonb_each_text($$ || CASE WHEN p_jsonb IS NULL THEN 'NULL' ELSE ''''||p_jsonb::TEXT||'''' END || $$)
         UNION SELECT * FROM jsonb_each_text('$$
         || (
           SELECT format('{ %s }', array_to_string((SELECT array_agg(format('"%1$s": ["%2$s"]', field, val)) FROM (
@@ -26,6 +26,7 @@ BEGIN
       ) data
     ) x
   $$;
+  -- RAISE DEBUG 'query: %', query;
 
   EXECUTE query INTO result;
 
