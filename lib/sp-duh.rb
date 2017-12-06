eac79717d51a286937538dae46cde9a6b5150c91
Eurico Inocencio <eurico.inocencio@gmail.com>
2017-12-06 14:29:33 +0000

    Adding copyright notices

--- lib/sp-duh.rb
+++ lib/sp-duh.rb
@@ -1,3 +1,23 @@
+#
+# Copyright (c) 2011-2017 Cloudware S.A. All rights reserved.
+#
+# This file is part of sp-duh.
+#
+# sp-duh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sp-duh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with sp-duh.  If not, see <http://www.gnu.org/licenses/>.
+#
+# encoding: utf-8
+#
 def require_silently(require_name)
   begin
     require require_name
