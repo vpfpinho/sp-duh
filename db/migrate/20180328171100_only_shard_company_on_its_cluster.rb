28069bf1ce05c7e4ee6770801f7be851d53f1b38
Joana Alexandra Neves <joana.neves@cldware.com>
2018-04-17 11:47:46 +0100

    Improving cluster control

--- db/migrate/20180328171100_only_shard_company_on_its_cluster.rb
+++ db/migrate/20180328171100_only_shard_company_on_its_cluster.rb
@@ -8,20 +8,19 @@ class OnlyShardCompanyOnItsCluster < ActiveRecord::Migration
         _current_cluster integer;
       BEGIN
 
-        SHOW cloudware.cluster INTO _current_cluster;
-        IF _current_cluster IS DISTINCT FROM NEW.cluster THEN
-          RAISE DEBUG 'Ignoring company for cluster % [current cluster id %]', NEW.cluster, _current_cluster;
-          RETURN NEW;
-        END IF;
-
-        RAISE NOTICE 'Sharding company [%] % - % - %', NEW.id, NEW.tax_registration_number, COALESCE(NEW.business_name, NEW.company_name, '<unnamed>'), NEW.use_sharded_company;
+        SHOW cloudware.cluster INTO _current_cluster; -- EXCEPTION if parameter is not set
 
         IF NEW.use_sharded_company THEN
-
           IF NULLIF(NEW.schema_name,'') IS NULL THEN
             NEW.schema_name := format('pt%1$s_c%2$s', NEW.tax_registration_number, NEW.id);
           END IF;
 
+          IF _current_cluster IS DISTINCT FROM NEW.cluster THEN
+            RAISE DEBUG 'Ignoring company for cluster % [current cluster id %]', NEW.cluster, _current_cluster;
+            RETURN NEW;
+          END IF;
+
+          RAISE NOTICE 'Sharding company [%] % - % - %', NEW.id, NEW.tax_registration_number, COALESCE(NEW.business_name, NEW.company_name, '<unnamed>'), NEW.use_sharded_company;
           -- Create company schema if necessary
           RAISE DEBUG 'Creating new schema "%"', NEW.schema_name;
           EXECUTE ('CREATE SCHEMA IF NOT EXISTS "' || NEW.schema_name || '";');
