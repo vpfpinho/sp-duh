2228c7e4eed623d6222f3bf4726dbe4bc9d4cf3c
João Ramos <poor666@gmail.com>
2019-05-13 16:40:51 +0100

    update common get certified software notice

--- db/functions/common/get_certified_software_notice.sql
+++ db/functions/common/get_certified_software_notice.sql
@@ -1,16 +1,26 @@
+--DROP FUNCTION common.get_certified_software_notice(integer, text);
 CREATE OR REPLACE FUNCTION common.get_certified_software_notice(
-  _company_id   INTEGER,
-  locale    TEXT DEFAULT 'pt_PT'
+  IN _company_id   INTEGER,
+  IN locale    TEXT DEFAULT 'pt_PT',
+  OUT certified_software_notice text,
+  OUT get_certified_software_notice text,
+  OUT document_certified_notice_non_hashed text,
+  OUT document_certified_notice text,
+  OUT document_certified_notice_short text
 )
-RETURNS text AS $BODY$
+
+RETURNS RECORD AS $BODY$
 DECLARE
-  _i18n_text  text;
   _product    text;
   _brand      text;
 BEGIN
-    SELECT product, brand FROM common.get_company_product_and_brand(_company_id) INTO _product, _brand;
-    _i18n_text := common.get_i18n('certified_software_notice_' || _product || '_' || _brand, locale);
-    
-    RETURN _i18n_text;
+  SELECT product, brand FROM common.get_company_product_and_brand(_company_id) INTO _product, _brand;
+  SELECT common.get_i18n('certified_software_notice_' || _product || '_' || _brand, locale)::TEXT AS certified_software_notice,
+          common.get_i18n('certified_software_notice_' || _product || '_' || _brand, locale)::TEXT AS get_certified_software_notice,
+          common.get_i18n('document_certified_notice_non_hashed_' || _product || '_' || _brand, locale)::TEXT AS document_certified_notice_non_hashed,
+          common.get_i18n('document_certified_notice_' || _product || '_' || _brand, locale)::TEXT AS document_certified_notice,
+          common.get_i18n('document_certified_notice_short_' || _product || '_' || _brand, locale)::TEXT AS document_certified_notice_short 
+          INTO certified_software_notice, get_certified_software_notice, document_certified_notice_non_hashed, document_certified_notice, document_certified_notice_short;
+  RETURN;
 END;
 $BODY$ LANGUAGE plpgsql IMMUTABLE;
