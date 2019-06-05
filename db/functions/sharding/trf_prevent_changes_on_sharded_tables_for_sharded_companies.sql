8aa79b152c7109e7cc7b795df6685e1e43fa7f99
Joana Alexandra Neves <joana.alexandra.neves@gmail.com>
2019-06-05 18:16:17 +0100

    Let's use this function to verify noyt only if company was sharded but also if it was moved non-sharded into another cluster

--- db/functions/sharding/trf_prevent_changes_on_sharded_tables_for_sharded_companies.sql
+++ db/functions/sharding/trf_prevent_changes_on_sharded_tables_for_sharded_companies.sql
@@ -5,6 +5,8 @@ RETURNS TRIGGER AS $BODY$
 DECLARE
   _stack         text;
   _company_id    integer;
+  _comp          record; 
+  _current_cluster integer;
 BEGIN
 
   GET DIAGNOSTICS _stack = PG_CONTEXT;
@@ -14,9 +16,17 @@ BEGIN
 
   EXECUTE 'SELECT ($1).company_id::integer' INTO _company_id USING (CASE TG_OP WHEN 'DELETE' THEN OLD ELSE NEW END);
 
-  IF (SELECT use_sharded_company FROM public.companies WHERE id = _company_id) THEN
+  SELECT use_sharded_company,cluster FROM public.companies WHERE id = _company_id INTO _comp;
+  IF _comp.use_sharded_company THEN
     RAISE restrict_violation
       USING MESSAGE = format('Company %1$L has already been sharded, can''t INSERT or UPDATE or DELETE records on unsharded tables' , _company_id),
+            TABLE = TG_TABLE_NAME;
+  END IF;
+
+  SHOW cloudware.cluster INTO _current_cluster;
+  IF _comp.cluster != _current_cluster THEN
+    RAISE restrict_violation
+      USING MESSAGE = format('Company %1$L is on another cluster, can''t INSERT or UPDATE or DELETE records from cluster %2$s' , _company_id, _comp.cluster),
             TABLE = TG_TABLE_NAME;
   END IF;
 
