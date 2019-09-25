439a5c20ffd002e0508b3f7c08e628979633ea99
Jorge Morais <jorge.morais@cldware.com>
2019-09-25 12:09:03 +0100

    force_ignore_duplicate_trigger_using_exception

---
+++ db/migrate/20190910170141_force_ignore_duplicate_trigger_using_exception.rb
@@ -0,0 +1,32 @@
+class ForceIgnoreDuplicateTriggerUsingException < ActiveRecord::Migration
+  def up
+    execute <<-'SQL'
+      CREATE OR REPLACE FUNCTION sharding.wrap_with_duplicate_check(
+        IN p_query        TEXT,
+        IN p_table_name   TEXT,
+        IN p_trigger_name TEXT
+      )
+      RETURNS TEXT AS $BODY$
+      DECLARE
+      BEGIN
+        RETURN format(
+          $RETURN$
+            DO $BLOCK$
+              BEGIN
+                  %1$s
+              EXCEPTION WHEN duplicate_object THEN
+                  RAISE DEBUG 'Ignoring duplicate %2$s::%3$s';
+              END;
+            $BLOCK$
+          $RETURN$,
+          p_query, p_table_name, p_trigger_name
+        );
+      END;
+      $BODY$ LANGUAGE plpgsql;
+    SQL
+  end
+
+  def down
+    puts "Not reverting"
+  end
+end
