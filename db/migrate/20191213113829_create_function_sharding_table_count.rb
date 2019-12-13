3ef541cfcbe932f9a62165ca5b918d1da74ef6ac
Joana Alexandra Neves <joana.alexandra.neves@gmail.com>
2019-12-13 11:52:30 +0000

    Created function sharding.table_count (text); to be used while creating DB seeds

---
+++ db/migrate/20191213113829_create_function_sharding_table_count.rb
@@ -0,0 +1,21 @@
+class CreateFunctionShardingTableCount < ActiveRecord::Migration
+  def up
+    execute <<-'SQL'
+      CREATE OR REPLACE FUNCTION sharding.table_count (
+        IN  p_table_name text,
+        OUT table_count  integer
+      ) RETURNS integer AS $BODY$
+      BEGIN
+        EXECUTE ('SELECT count(1) FROM '||p_table_name) INTO table_count;
+        RETURN;
+      END;
+      $BODY$ LANGUAGE 'plpgsql';
+    SQL
+  end
+
+  def down
+    execute <<-'SQL'
+      DROP FUNCTION IF EXISTS sharding.table_count (text);
+    SQL
+  end
+end
